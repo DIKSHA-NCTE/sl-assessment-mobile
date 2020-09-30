d9804553010a9450f1950060ae5de894219bcd52
swayangjit <swayangjit029@gmail.com>
2020-09-30 20:42:40 +0530

    Issue #SS-000 chore: Updated build.sh

--- build.sh
+++ build.sh
@@ -11,30 +11,32 @@ SUNBIRD_CORDOVA_COUNTER=0
 
 npm install
 
-file="./build_config"
-while IFS="=" read -r key value; do
-  case "$key" in
-    '#'*) ;;
-    'cordova'*)
-      CORDOVA[$CORDOVA_COUNTER]=$value
-      CORDOVA_COUNTER=$((CORDOVA_COUNTER+1));;
-    'sunbird-cordova'*)
-      SUNBIRD_CORDOVA[$SUNBIRD_CORDOVA_COUNTER]=$value
-      SUNBIRD_CORDOVA_COUNTER=$((SUNBIRD_CORDOVA_COUNTER+1));
-  esac
-done < "$file"
+# file="./build_config"
+# while IFS="=" read -r key value; do
+#   case "$key" in
+#     '#'*) ;;
+#     'cordova'*)
+#       CORDOVA[$CORDOVA_COUNTER]=$value
+#       CORDOVA_COUNTER=$((CORDOVA_COUNTER+1));;
+#     'sunbird-cordova'*)
+#       SUNBIRD_CORDOVA[$SUNBIRD_CORDOVA_COUNTER]=$value
+#       SUNBIRD_CORDOVA_COUNTER=$((SUNBIRD_CORDOVA_COUNTER+1));
+#   esac
+# done < "$file"
 
 
-for cordova_plugin in "${CORDOVA[@]}"
-do
-  ionic cordova plugin add $cordova_plugin
-done
+# for cordova_plugin in "${CORDOVA[@]}"
+# do
+#   ionic cordova plugin add $cordova_plugin
+# done
 
-for cordova_plugin in "${SUNBIRD_CORDOVA[@]}"
-do
-  ionic cordova plugin add $cordova_plugin
-done
+# for cordova_plugin in "${SUNBIRD_CORDOVA[@]}"
+# do
+#   ionic cordova plugin add $cordova_plugin
+# done
 
+ionic cordova plugin add cordova-plugin-inappbrowser
+ionic cordova plugin add https://github.com/Sunbird-Ed/sb-cordova-plugin-customtabs.git --variable URL_SCHEME=in.gov.samiksha.diksha.app  --variable URL_HOST=mobile
 
 rm -rf platforms
 
